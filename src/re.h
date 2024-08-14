7de6751bf83ce9b8d834206f577263d6362a30a7
rick2600 <ricks2600@gmail.com>
2024-08-14 17:45:29 -0300

    match colors

--- src/re.h
+++ src/re.h
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+#define RESET   "\033[0m"         // Reset to default color
+#define BLACK   "\033[1;30m"      // Black
+#define RED     "\033[1;31m"      // Red
+#define GREEN   "\033[1;32m"      // Green
+#define YELLOW  "\033[1;33m"      // Yellow
+#define BLUE    "\033[1;34m"      // Blue
+#define MAGENTA "\033[1;35m"      // Magenta
+#define CYAN    "\033[1;36m"      // Cyan
+#define WHITE   "\033[1;37m"      // White
 
 
 typedef struct re_submatch {
